d69fe47c4d8707c1d71dc5f9b1f7b7692468cfd5
Cristhofer Pincetti <86972354+BrainerVirus@users.noreply.github.com>
2024-09-07 19:08:16 -0400

    chore: set up eslint, prettier,  vscode configurations

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NativeWindStyleSheet } from "nativewind";
+import { StatusBar } from "expo-status-bar"
+import { NativeWindStyleSheet } from "nativewind"
+import { Text, View } from "react-native"
 
 export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View className="flex-1 items-center justify-center bg-white">
+			<Text>Open up App.tsx to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	)
 }
 
 NativeWindStyleSheet.setOutput({
-  default: "native",
-});
+	default: "native",
+})
