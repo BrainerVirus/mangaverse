ad3a5d6a5067731e7e1260550523d69563b7a974
Cristhofer Pincetti <github.annotate387@passmail.net>
2026-02-15 11:25:03 -0300

    fix(HorizontalSection): add top padding to scroll view content container

--- components/HorizontalSection.tsx
+++ components/HorizontalSection.tsx
@@ -62,7 +62,7 @@ export function HorizontalSection({
 				horizontal
 				showsHorizontalScrollIndicator={false}
 				className="mt-4"
-				contentContainerStyle={{ paddingRight: pagePadding, columnGap: gap }}
+				contentContainerStyle={{ paddingRight: pagePadding, columnGap: gap, paddingTop: 8 }}
 				scrollEventThrottle={120}
 				onScroll={onScroll}
 			>
